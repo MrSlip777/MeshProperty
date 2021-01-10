7fd7bf9282b79594125fbe54718eb69886354257
slip <mrslipgamecreate01@gmail.com>
2021-01-10 10:46:19 +0900

    Draft

---
+++ README.md
@@ -0,0 +1,27 @@
+# SummarizeMeshProperty.unitypackage
+## 概要
+対象モデルのマテリアル数、頂点数、ポリゴン数を表示するUnityエディタ拡張<br>
+
+# 0 準備
+本スクリプトはUnity上で動作します。<br>
+(SkinnedMeshRenderer,MeshFilter,MeshRendererに対応します)<br>
+
+[動作確認済みの環境]<br>
+・Unity 2019 4.0f1<br>
+・UniVRM 0.56.3<br>
+
+## 0.1 導入
+1．Unityを起動する<br>
+2．unitypackageをUnityエディタへドラッグ&ドロップする<br>
+3．Unity上でインポートウインドウが表示されるのでインポートを押下する<br>
+
+## 0.2 メニュー一覧
+メニューバーにてUniVRM-(バージョン名)を開くと以下のメニューが表示されます。<br>
+
+・MeshProperty<br>
+　・Show<br>
+
+# 1 マテリアル数、頂点数、ポリゴン数の表示
+1.MeshProperty→Showを選択し、ダイアログを表示する<br>
+2.対象モデル欄に対象モデルを設定する(Hierarchyからドラッグ&ドロップする)
+3.Showボタンを押下する
---
+++ SummarizeMeshProperty.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * 対象モデルの概要を表示するUnityエディタ拡張
+ *
+ * (C)2021 slip
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ * [Twitter]: https://twitter.com/kjmch2s/
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このスクリプトはもうあなたのものです。
+ * 
+ */
+
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class SummarizeMeshProperty: ScriptableWizard
+{
+
+    [SerializeField]
+    //対象になるモデル
+    private GameObject targetModel;
+
+    private int materials = 0;
+    private int vertices = 0;
+    private int polygons = 0; 
+
+    public static void CreateWizard()
+    {
+        var wiz = ScriptableWizard.DisplayWizard<SummarizeMeshProperty>(
+            "SummarizeMeshProperty", "Show");
+        var go = Selection.activeObject as GameObject;
+
+    }
+
+    void OnWizardCreate()
+    {
+        //SkinnedMeshRenderer
+        SkinnedMeshRenderer[] skinnedMeshRenderers
+         = targetModel.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+        foreach(SkinnedMeshRenderer skin in skinnedMeshRenderers){
+            //マテリアル数
+            materials += skin.sharedMaterials.Length;
+            //頂点数
+            vertices += skin.sharedMesh.vertices.Length;
+            //ポリゴン数
+            polygons += skin.sharedMesh.triangles.Length / 3;
+
+        }
+
+        //MeshRenderer
+        MeshRenderer[] meshRenderers
+         = targetModel.GetComponentsInChildren<MeshRenderer>();
+
+        foreach(MeshRenderer renderer in meshRenderers){
+            //マテリアル数
+            materials += renderer.sharedMaterials.Length;
+        }
+
+        //MeshFilter
+        MeshFilter[] meshFilters
+         = targetModel.GetComponentsInChildren<MeshFilter>();
+
+        foreach(MeshFilter filter in meshFilters){
+            //頂点数
+            vertices += filter.sharedMesh.vertices.Length;
+            //ポリゴン数
+            polygons += filter.sharedMesh.triangles.Length / 3;
+        }
+
+        EditorUtility.DisplayDialog("Summary", "materials/マテリアル数：" + materials
+        + "\nverts/頂点数       ：" + vertices
+        + "\npolygons/ポリゴン数：" + polygons, "OK");
+    }    
+
+    void OnDestroy(){
+        //終了時の処理を記載する
+        //DestroyImmediate(m_Wizard);
+    }   
+}
+
+public static class SummarizeMeshPropertyMenu
+{
+    const string ADD_OPTIONOBJECT_KEY = "MeshProperty/Show";
+
+    [MenuItem(ADD_OPTIONOBJECT_KEY)]
+    private static void Menu()
+    {
+        SummarizeMeshProperty.CreateWizard();
+    }
+}
